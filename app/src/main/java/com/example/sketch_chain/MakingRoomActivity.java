539569249ba509bdf5e6a328f765c3570ced5d05
Original-Kimchi <39785268+Original-Kimchi@users.noreply.github.com>
2019-09-17 18:53:29 +0900

    Update MakingRoomActivity

---
+++ app/src/main/java/com/example/sketch_chain/MakingRoomActivity.java
@@ -0,0 +1,13 @@
+package com.example.sketch_chain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MakingRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_making_room);
+    }
+}
