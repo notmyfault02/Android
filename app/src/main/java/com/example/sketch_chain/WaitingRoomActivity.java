92a555c1e6bd1d21fc10df190391d34178b7cf5e
Original-Kimchi <39785268+Original-Kimchi@users.noreply.github.com>
2019-09-27 14:03:08 +0900

    Update xml

--- app/src/main/java/com/example/sketch_chain/WaitingRoomActivity.java
+++ app/src/main/java/com/example/sketch_chain/WaitingRoomActivity.java
@@ -8,6 +8,6 @@ public class WaitingRoomActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_waiting_room);
+        setContentView(R.layout.activity_playing_room);
     }
 }
