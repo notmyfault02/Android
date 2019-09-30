967cd186a9db09537f27afdfa6b89d4846ca6a7b
Yeongrae Kim <rayray.yoyo@gmail.com>
2019-09-30 23:51:01 +0900

    Update search

--- app/src/main/java/com/example/sketch_chain/RoomListActivity.java
+++ app/src/main/java/com/example/sketch_chain/RoomListActivity.java
@@ -1,5 +1,6 @@
 package com.example.sketch_chain;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,10 +14,16 @@ public class RoomListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_room_list);
 
         ImageView cancelBtn = findViewById(R.id.roomlist_cancel_iv);
+        ImageView searchBtn = findViewById(R.id.roomlist_search_iv);
 
     }
 
     public void cancel(View view) {
         finish();
+    }
+
+    public void goSearch(View view) {
+        Intent intent = new Intent(getApplicationContext(), SearchRoomActivity.class);
+        startActivity(intent);
     }
 }
