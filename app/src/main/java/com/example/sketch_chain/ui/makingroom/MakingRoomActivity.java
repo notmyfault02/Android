f906f591bc1a14e7c7c9e92707c908d1e065b7e4
Yeongrae Kim <rayray.yoyo@gmail.com>
2019-10-08 14:40:49 +0900

    Update making, search

--- app/src/main/java/com/example/sketch_chain/ui/makingroom/MakingRoomActivity.java
+++ app/src/main/java/com/example/sketch_chain/ui/makingroom/MakingRoomActivity.java
@@ -4,9 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.example.sketch_chain.R;
 import com.example.sketch_chain.ui.gameplay.PlayingRoomActivity;
@@ -14,8 +13,7 @@ import com.example.sketch_chain.ui.gameplay.PlayingRoomActivity;
 public class MakingRoomActivity extends AppCompatActivity {
 
     private EditText roomName;
-    private ImageView confirmIv;
-    private TextView confirmTv;
+    private Button makingBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +21,10 @@ public class MakingRoomActivity extends AppCompatActivity {
         setContentView(R.layout.activity_making_room);
 
         roomName = findViewById(R.id.room_name_et);
-        confirmIv = findViewById(R.id.confirm_background_iv);
-        confirmTv = findViewById(R.id.confirm_button_tv);
+        makingBtn = findViewById(R.id.making_confirm_btn);
 
-        confirmIv.setOnClickListener(new View.OnClickListener() {
+
+        makingBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), PlayingRoomActivity.class);
--- app/src/main/java/com/example/sketch_chain/ui/roomlist/SearchRoomActivity.java
+++ app/src/main/java/com/example/sketch_chain/ui/roomlist/SearchRoomActivity.java
@@ -1,15 +1,33 @@
 package com.example.sketch_chain.ui.roomlist;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.sketch_chain.R;
 
 public class SearchRoomActivity extends AppCompatActivity {
+    private Button searchBtn;
+    private boolean mBtnClickToggle;
+    private EditText searchEt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_room);
+
+        searchEt = findViewById(R.id.search_et);
+        searchBtn = findViewById(R.id.search_confirm_btn);
+        if (searchEt.getText().toString() == null) {
+            searchBtn.setEnabled(false);
+        } else {
+            searchBtn.setEnabled(true);
+        }
+    }
+
+    public void cancel(View view) {
+        finish();
     }
 }
