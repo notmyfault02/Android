decb71cd3a8c59870422bfc879bdb2c2780e05c9
Yeongrae Kim <rayray.yoyo@gmail.com>
2019-10-25 14:10:09 +0900

    Update theme

---
+++ presentation/src/main/java/com/example/sketch_chain/ui/store/StoreActivity.java
@@ -0,0 +1,15 @@
+package com.example.sketch_chain.ui.store;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.sketch_chain.R;
+
+public class StoreActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store);
+    }
+}
