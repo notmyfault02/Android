98b611e3be17b3d5340dfe2220be5b364909450d
Yeongrae Kim <rayray.yoyo@gmail.com>
2019-10-29 15:42:55 +0900

    Update api

--- data/src/main/java/com/notmyfault02/data/remote/Api.java
+++ data/src/main/java/com/notmyfault02/data/remote/Api.java
@@ -1,13 +1,39 @@
 package com.notmyfault02.data.remote;
 
+import com.newgram.domain.entity.UserEntity;
+import com.notmyfault02.data.entity.RoomData;
 import com.notmyfault02.data.entity.UserData;
 
 import java.util.List;
 
 import io.reactivex.Flowable;
+import retrofit2.Response;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface Api {
-    @GET("/getRanking")
+    @GET("/user/info")
+    Flowable<UserEntity> getUser();
+
+    @GET("/user/rank")
     Flowable<List<UserData>> getRanking();
+
+    @POST("/room")
+    Flowable<Response<RoomData>>
+    makeRoom(@Body RoomData body);
+
+    @GET("/room/search")
+    Flowable<RoomData>
+    searchRoom(@Query("title") String roomName);
+
+    @GET("/room/join")
+    Flowable<RoomData>
+    joinRoom(@Query("roomId") int roomId);
+
+    @POST("/user/item")
+    Flowable<Response<String>>
+    buyItem(@Query("type") List<Integer> item);
+
 }
